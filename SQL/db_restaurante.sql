41ed070bee8c01f06ee3c2bb86133d7a06c0ddcc
Myngatte <zhouming2004@gmail.com>
2024-11-05 17:58:33 +0100

    cambios BBDD

--- SQL/db_restaurante.sql
+++ SQL/db_restaurante.sql
@@ -3,7 +3,7 @@ CREATE DATABASE db_restaurante;
 USE db_restaurante;
 
 CREATE TABLE tbl_camarero (
-    id_camarero INT NOT NULL PRIMARY KEY,
+    id_camarero INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name_camarero VARCHAR(30) NOT NULL,
     surname_camarero VARCHAR(30) NOT NULL,
     username_camarero VARCHAR(30) NOT NULL,
@@ -11,16 +11,16 @@ CREATE TABLE tbl_camarero (
 );
 
 CREATE TABLE tbl_salas (
-    id_salas INT NOT NULL PRIMARY KEY,
+    id_salas INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name_sala VARCHAR(15) NOT NULL,
     tipo_sala ENUM("Comedor", "Terraza", "Privado") NOT NULL
 );
 
 CREATE TABLE tbl_mesas (
-    id_mesa INT NOT NULL PRIMARY KEY,
-    n_asientos INT(2) NOT NULL,
+    id_mesa INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    n_asientos INT NOT NULL,
     id_sala INT NOT NULL,
-    estado_sala ENUM("A", "NA") NOT NULL,
+    estado_sala ENUM("A", "NA") NOT NULL DEFAULT "NA",
     assigned_by INT NULL,
     assigned_to VARCHAR(30) NULL
 );
@@ -28,28 +28,57 @@ CREATE TABLE tbl_mesas (
 ALTER TABLE tbl_mesas ADD CONSTRAINT fk_sala_mesa FOREIGN KEY (id_sala) REFERENCES tbl_salas(id_salas);
 ALTER TABLE tbl_mesas ADD CONSTRAINT fk_mesa_asignada FOREIGN KEY (assigned_by) REFERENCES tbl_camarero(id_camarero);
 
-INSERT INTO tbl_camarero (id_camarero, name_camarero, surname_camarero, username_camarero, pwd_camarero)
+INSERT INTO tbl_camarero (name_camarero, surname_camarero, username_camarero, pwd_camarero)
 VALUES
-(1, 'Carlos', 'García', 'cgarcia', SHA2('Camarero123.', 256)),
-(2, 'Laura', 'Martínez', 'lmartinez', SHA2('Camarero123.', 256)),
-(3, 'Ana', 'Sánchez', 'asanchez', SHA2('Camarero123.', 256)),
-(4, 'Jorge', 'Hernández', 'jhernandez', SHA2('Camarero123.', 256)),
-(5, 'Elena', 'López', 'elopez', SHA2('Camarero123.', 256));
+('Carlos', 'García', 'cgarcia', SHA2('Camarero123.', 256)),
+('Laura', 'Martínez', 'lmartinez', SHA2('Camarero123.', 256)),
+('Ana', 'Sánchez', 'asanchez', SHA2('Camarero123.', 256)),
+('Jorge', 'Hernández', 'jhernandez', SHA2('Camarero123.', 256)),
+('Elena', 'López', 'elopez', SHA2('Camarero123.', 256));
 
 -- Inserciones en la tabla tbl_salas
-INSERT INTO tbl_salas (id_salas, name_sala, tipo_sala)
+INSERT INTO tbl_salas (name_sala, tipo_sala)
 VALUES
-(1, 'Principal', 'Comedor'),
-(2, 'Terraza', 'Terraza'),
-(3, 'Salón VIP', 'Privado'),
-(4, 'Comedor 2', 'Comedor'),
-(5, 'Jardín', 'Terraza');
+('Comedor 1', 'Comedor'),
+('Terraza 1', 'Terraza'),
+('Salón VIP', 'Privado'),
+('Comedor 2', 'Comedor'),
+('Jardín', 'Terraza'),
+('Terraza 2', 'Terraza'),
+('Salón VIP 2', 'Privado'),
+('Salón romántico', 'Privado'),
+('Naturaleza', 'Privado');
 
 -- Inserciones en la tabla tbl_mesas
-INSERT INTO tbl_mesas (id_mesa, n_asientos, id_sala, estado_sala)
-VALUES
-(1, 4, 1, 'NA'),
-(2, 6, 1, 'NA'),
-(3, 2, 2, 'NA'),
-(4, 8, 3, 'NA'),
-(5, 4, 4, 'NA');
+INSERT INTO tbl_mesas (n_asientos, id_sala) VALUES
+(4, 1),
+(6, 1),
+(8, 1),
+(2, 1),
+(4, 2),
+(6, 2),
+(8, 2),
+(2, 3),
+(4, 3),
+(6, 3),
+(8, 3),
+(4, 4),
+(6, 4),
+(2, 4),
+(8, 5),
+(4, 5),
+(6, 5),
+(2, 5),
+(4, 6),
+(6, 6),
+(8, 6),
+(2, 7),
+(4, 7),
+(6, 7),
+(8, 8),
+(4, 8),
+(6, 8),
+(2, 9),
+(4, 9),
+(6, 9);
+
