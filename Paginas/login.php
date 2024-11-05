65f87aa1ba5d91d007eab70aeb45bc5694b92c43
alexventura <468140.joan23@fje.edu>
2024-11-05 18:26:46 +0100

    Validaciones JS

--- Paginas/login.php
+++ Paginas/login.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,16 +9,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/estilos.css">
+    <script src="../JS/validaciones.js"></script>
     <title>Bienvenido</title>
 </head>
 <body>
-    <form id="login" class="login">
+    <form id="login" class="login" method="POST" action="">
         <label>Nombre de usuario:</label>
-        <input type="text" name="username">
+        <input type="text" id="username" name="username">
         <br>
         <br>
         <label>Contraseña:</label>
-        <input type="password" name="pwd">
+        <input type="password" id="pwd" name="pwd">
         <br>
         <br>
         <input type="submit" value="Enviar">
