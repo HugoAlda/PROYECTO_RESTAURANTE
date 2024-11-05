1822f738711822b912fbe0a4630c9b1871c8353d
alexventura <468140.joan23@fje.edu>
2024-11-05 16:38:28 +0100

    Estructura base

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido</title>
+</head>
+<body>
+    <form id="login" class="login">
+        <label>Nombre de usuario</label>
+        <input type="text" name="username">
+        <br>
+        <label>Contraseña</label>
+        <input type="password" name="pwd">
+    </form>
+</body>
+</html>
