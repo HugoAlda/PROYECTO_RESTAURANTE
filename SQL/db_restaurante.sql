eaee4d04213c114e5e5d9bf1c3b8a30ef062fe5f
ErikPenas02 <epenyasandrea@gmail.com>
2024-11-05 17:32:03 +0100

    Update db_restaurante.sql

--- SQL/db_restaurante.sql
+++ SQL/db_restaurante.sql
@@ -27,3 +27,29 @@ CREATE TABLE tbl_mesas (
 
 ALTER TABLE tbl_mesas ADD CONSTRAINT fk_sala_mesa FOREIGN KEY (id_sala) REFERENCES tbl_salas(id_salas);
 ALTER TABLE tbl_mesas ADD CONSTRAINT fk_mesa_asignada FOREIGN KEY (assigned_by) REFERENCES tbl_camarero(id_camarero);
+
+INSERT INTO tbl_camarero (id_camarero, name_camarero, surname_camarero, username_camarero, pwd_camarero)
+VALUES
+(1, 'Carlos', 'García', 'cgarcia', SHA2('Camarero123.', 256)),
+(2, 'Laura', 'Martínez', 'lmartinez', SHA2('Camarero123.', 256)),
+(3, 'Ana', 'Sánchez', 'asanchez', SHA2('Camarero123.', 256)),
+(4, 'Jorge', 'Hernández', 'jhernandez', SHA2('Camarero123.', 256)),
+(5, 'Elena', 'López', 'elopez', SHA2('Camarero123.', 256));
+
+-- Inserciones en la tabla tbl_salas
+INSERT INTO tbl_salas (id_salas, name_sala, tipo_sala)
+VALUES
+(1, 'Principal', 'Comedor'),
+(2, 'Terraza', 'Terraza'),
+(3, 'Salón VIP', 'Privado'),
+(4, 'Comedor 2', 'Comedor'),
+(5, 'Jardín', 'Terraza');
+
+-- Inserciones en la tabla tbl_mesas
+INSERT INTO tbl_mesas (id_mesa, n_asientos, id_sala, estado_sala)
+VALUES
+(1, 4, 1, 'NA'),
+(2, 6, 1, 'NA'),
+(3, 2, 2, 'NA'),
+(4, 8, 3, 'NA'),
+(5, 4, 4, 'NA');
