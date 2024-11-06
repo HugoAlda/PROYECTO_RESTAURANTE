3bedbbf169f2abc9e488e3de5998bfc0408fc5a7
alexventura <468140.joan23@fje.edu>
2024-11-06 19:26:21 +0100

    Validaciones JS acabada

--- index.php
+++ index.php
@@ -9,16 +9,18 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./CSS/estilos.css">
-    <script src="../JS/validaciones.js"></script>
+    <script src="JS/validaciones.js"></script>
     <title>Bienvenido</title>
 </head>
 <body>
-    <form id="login" class="login" method="POST" action="proceso_login.php">
+    <form id="login" class="login" method="POST" action="Procesos/procesoLogin.php">
         <label>Nombre de usuario:</label>
         <input type="text" id="username" name="username" placeholder="Nombre de usuario">
+        <span class="error" id="errorUsername"></span>
         <br>
         <label>Contraseña:</label>
         <input type="password" id="pwd" name="pwd" placeholder="Contraseña">
+        <span class="error" id="errorContraseña"></span>
         <br>
         <input type="submit" value="Enviar" id="enviar">
     </form>
