e0413331ae562d8bc49fff3bc9d9e37e70ba050c
ErikPenas02 <epenyasandrea@gmail.com>
2024-11-06 18:05:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ session_start();
     <title>Bienvenido</title>
 </head>
 <body>
-    <form id="login" class="login" method="POST" action="./Procesos/procesoLogin.php">
+    <form id="login" class="login" method="POST" action="/Procesos/procesoLogin.php">
         <label>Nombre de usuario:</label>
         <input type="text" id="username" name="username" placeholder="Nombre de usuario" <?php if(isset($_GET["error"]) && $_GET["error"] === "datosMal"){echo "style='border-color: red;'";} ?>>
         <br>
