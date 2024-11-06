d158224ee9ea93c249e0f6678ade0ae1442cce4e
alexventura <468140.joan23@fje.edu>
2024-11-06 17:03:52 +0100

    Ruta

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/estilos.css">
+    <link rel="stylesheet" href="./CSS/estilos.css">
     <script src="../JS/validaciones.js"></script>
     <title>Bienvenido</title>
 </head>
